38a4861681a2924a1b60ac76c0ae43def4bb8b47
Huy LV <huylv6635@co-well.com.vn>
2018-11-29 11:35:16 +0700

    Learning VueJS

---
+++ resources/assets/js/components/ProductComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="product-component">
+        {{ message }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Hello World!'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .product-component {
+        color: red;
+    }
+</style>
